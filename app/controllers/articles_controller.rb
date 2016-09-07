66e71debf6ead26231a09f697f405a0ccd832540
S. DOSSETT SWIFT <sdossettswift@gmail.com>
2016-09-07 11:04:28 -0500

    categories for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :find_article, only: [:show, :update, :edit, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @articles = Article.all.order("created_at DESC")
   end
@@ -38,7 +39,7 @@ class ArticlesController < ApplicationController
 
   private
   def articles_params
-    params.require(:article).permit(:title, :content)
+    params.require(:article).permit(:title, :content, :category_id)
   end
 
   def find_article
